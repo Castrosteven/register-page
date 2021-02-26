14e887cd4b2059e8b5b07c11f347bee0afbe7dd8
castrosteven <castrostevenyc@gmail.com>
2021-02-26 02:06:05 -0500

    update BrowserDetect

--- src/components/BrowserDetect.jsx
+++ src/components/BrowserDetect.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 
 const BrowserDetect = () => {
-  const [ip, setIp] = useState({});
+  const [ip, setIp] = useState(null);
   const [os, setos] = useState("");
 
   useEffect(() => {
@@ -28,11 +28,22 @@ const BrowserDetect = () => {
   return (
     <>
       <div className="card" style={{ width: "13rem" }}>
-        <div className="card-body">
-          <p>Your Operating System is {os}</p>
-          <p>Your IP address is: {ip.ip}</p>
-          <p> Your hostname is : {ip.hostname} </p>
-        </div>
+        {ip && (
+          <div className="card-body">
+            <p>Your Operating System is {os}</p>
+            <p>Your IP address is: {ip.ip}</p>
+            <p> Your hostname is : {ip.hostname} </p>
+            <p> Your RPC Status is {ip.rcpStatus && "GOOD"} </p>
+          </div>
+        )}
+        {!ip && (
+          <div className="card-body">
+            <p>
+              OBTAINING HARDWWARE INFORMATION PLEASE WAIT <br />
+              LOADING......
+            </p>
+          </div>
+        )}
       </div>
     </>
   );
